f1b5dc59a3d217e482b34c607727a9cce2c3a0d2
Karen Harutyunyan <karenharutyunyan575@gmail.com>
2019-01-21 23:22:55 +0400

    My first commit for task

---
+++ app/src/main/java/com/example/taskui/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.taskui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
